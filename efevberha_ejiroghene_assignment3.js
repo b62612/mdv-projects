064a90a4b0a041769a71ec538b955d6727d18850
Ejiroghene <ejay.efe@gmail.com>
2013-08-21 18:15:52 -0400

    create method procedure for number of surplus players

--- efevberha_ejiroghene_assignment3.js
+++ efevberha_ejiroghene_assignment3.js
@@ -1,1 +1,52 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+{
+// Ejiroghene Efevberha
+// SDI 1308
+// Deliverable 3
+// Players to Market
+
+// my global variables
+var surplusPlayers;
+	//properties (in arrays and objects)
+	//methods (function)
+	//methods (procedure)
+var obj2;
+	//methods (accessor)
+	// methods (mutator)
+var jsonData;
+var obj4
+var obj5
+
+								{
+surplusPlayers = {
+	   "shirtNumber": 49,
+	   "name": "Sunday Mba",
+	   "position": "midfielder",
+	   "age": 24,
+	   "height": "1.94m",
+	   "isPlayerInjured": false,
+	   "goalsPreviouSeasons": [11, 16, 9],
+	   
+	   
+
+
+
+
+}
+
+// my method procedure 
+console.log ("NUMBER OF PLAYERS FOR THE MARKET");
+var surplus;
+var requiredSurplus = 3;
+var checkSurplus = function(surplus) {
+         if  (surplus < requiredSurplus) {   
+             console.log ("We do not have the required number of players considered suplus. Hence, we will keep the " + surplus + " players we have, off the market."); 
+        }
+         else {
+             console.log ("We have "+ surplus + " players available to market in this transfer window");
+        }
+    };
+checkSurplus(2);
+console.log (" ");
+
+};
